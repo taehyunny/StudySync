6b45d4cc2bc3093f2ed20a877e045ef45af7c4d5
Taehyun <taehyunny0312@gmail.com>
2026-05-07 11:30:49 +0900

    test ver 0.0.3

--- client/include/network/ClientTransportConfig.h
+++ client/include/network/ClientTransportConfig.h
@@ -11,7 +11,7 @@ struct ClientTransportConfig {
     ClipStoreKind clip_store = ClipStoreKind::LocalClaimCheck;
 
     // Main server uses HTTP for auth, sessions, logs, and claim-check metadata.
-    std::string main_server_url = "http://10.10.10.100:8080";
+    std::string main_server_url = "http://10.10.10.130:8080";
 
     // AI server uses TCP for frame upload and analysis responses.
     std::string ai_server_host = "10.10.10.50";
